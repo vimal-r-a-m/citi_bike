cff1a71d92515652422f30c1a8c4c76feff59ce2
vimal-r-a-m <vimalram8595@gmail.com>
2026-09-11 00:12:05 +0530

    feat: manually ingest monthly Citi Bike data into MinIO Bronze Bucket
    chore: Added requirements.txt for project dependencies.

---
+++ scripts/inspect_bronze.py
@@ -0,0 +1,20 @@
+import zipfile
+from pathlib import Path
+
+bronze_dir = Path("data/raw")
+extract_dir = Path("data/extracted")
+
+for zip_file in bronze_dir.rglob("*.zip"):
+
+    # Find where the zip is relative to data/raw
+    relative_path = zip_file.relative_to(bronze_dir)
+
+    # Keep the same parent folder structure
+    output_dir = extract_dir / relative_path.parent / zip_file.stem
+
+    output_dir.mkdir(parents=True, exist_ok=True)
+
+    print(f"Extracting: {zip_file} -> {output_dir}")
+
+    with zipfile.ZipFile(zip_file, "r") as zip_ref:
+        zip_ref.extractall(output_dir)
